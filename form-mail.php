5329988291b6084ecdb418866cede2deb2f37453
Taylor <crtcreative@gmail.com>
2017-04-02 21:48:38 -0700

    initial commit

---
+++ form-mail.php
@@ -0,0 +1,39 @@
+<?php
+
+$EmailFrom = "Grove App";
+$EmailTo = "Berwyn425@gmail.com";
+$Subject = "Web Inquiry";
+$Name = Trim(stripslashes($_POST['Name']));  
+$Email = Trim(stripslashes($_POST['Email'])); 
+$Feedback = Trim(stripslashes($_POST['Feedback']));  
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Feedback: ";
+$Body .= $Feedback;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+}
+?>
